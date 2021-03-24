88dd0b5b0ba530c123270e20fd7fa1cce04e0e0d
tdds <tdds@localhost.localdomain>
2021-03-25 04:55:08 +0700

    Init db

--- app.rb
+++ app.rb
@@ -2,6 +2,25 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/contrib/all'
+require 'sqlite3'
+
+
+
+			def init_db
+
+				@db = SQLite3::Database.new 'leprosorium.db'
+				@db.results_as_hash = true
+
+			end
+
+
+
+before do
+	init_db
+end
+
+
+
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -10,3 +29,9 @@ end
 get '/new' do
 	erb :new 
 end
+
+post '/new' do
+	@content = params[:content]
+
+	erb "you typed #{@content}"
+	end
